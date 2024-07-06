0ff493fe5ed5f1f4eb4e4d0da6b6abecbbec02c3
Patryk Okomoński <patricansk@gmail.com>
2024-07-06 17:08:48 +0200

    Stworzenie projektu. Pogoda jest straszna. 30 stopni w cieniu.

---
+++ src/main/java/pl/coderslab/projektklinika/ProjektKlinikaApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.projektklinika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektKlinikaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektKlinikaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/coderslab/projektklinika/ProjektKlinikaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.projektklinika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektKlinikaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
